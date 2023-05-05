f75b48255ddc9fc1d5acf0e414658ee10dce28b9
Torra Tran <pandapunch@Torras-MacBook-Air.local>
2023-05-04 17:11:50 -0700

    done

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const coffeeMenu = require("./coffee_data");
 
+//Print an array of all the drinks on the menu.
+
 const Name = (drink) => {
   return drink.name;
 };
 console.log(coffeeMenu.map(Name));
+
+//Print an array of drinks that cost 5 and under.
 
 const underfive = (drink) => {
   return drink.price <= 5;
@@ -11,11 +15,15 @@ const underfive = (drink) => {
 const drinkunderfive = coffeeMenu.filter(underfive);
 console.table(drinkunderfive);
 
+//Print an array of drinks that are priced at an even number.
+
 const onlyeven = (drink) => {
   return drink.price % 2 === 0;
 };
 const drinkonlyeven = coffeeMenu.filter(onlyeven);
 console.table(drinkonlyeven);
+
+//Print the total if you were to order one of every drink.
 
 const sumTotalPrice = (accumulator, currentValue) => {
   return accumulator + currentValue.price;
@@ -23,11 +31,15 @@ const sumTotalPrice = (accumulator, currentValue) => {
 const TotalPrice = coffeeMenu.reduce(sumTotalPrice);
 console.log(TotalPrice);
 
+//Print an array with all the drinks that are seasonal.
+
 const onlyseasonal = (drink) => {
   return drink.seasonal === true;
 };
 const drinkonlyseasonal = coffeeMenu.filter(onlyseasonal);
 console.table(drinkonlyseasonal);
+
+//Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
 
 const seasonal = (drink) => {
   return { ...drink, name: `${drink.name} with imported beans` };
